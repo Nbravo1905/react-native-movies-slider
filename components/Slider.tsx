5097f32dbf88a6a607f49615a96cf8d9ee98c09e
Nino <nbravo1905@gmail.com>
2024-09-09 14:39:59 -0500

    finish autoplay

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,10 +1,12 @@
-import { FlatList, StyleSheet, Text, View, ViewToken } from 'react-native'
-import React, { useRef, useState } from 'react'
+import { Dimensions, FlatList, StyleSheet, Text, View, ViewToken } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { ImageSliderType } from '@/data/SliderData'
 import SliderItem from '@/components/SliderItem'
-import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
+import Animated, { scrollTo, useAnimatedRef, useAnimatedScrollHandler, useDerivedValue, useSharedValue } from 'react-native-reanimated'
 import Pagination from './Pagination'
+
+const { width } = Dimensions.get('screen');
 
 type Props = {
   moviesSlider: ImageSliderType[]
@@ -15,10 +17,38 @@ const Slider = ({ moviesSlider }: Props) => {
   const scrollX = useSharedValue(0);
   const [paginationIndex, setPaginationIndex] = useState(0);
   const [data, setData] = useState(moviesSlider);
+  const ref = useAnimatedRef<Animated.FlatList<any>>();
+  const [isAutoPlay, setIsAutoPlay] = useState(true);
+  const interval = useRef<NodeJS.Timeout>();
+  const offset = useSharedValue(0);
+
+
+  useEffect(() => {
+    
+    if( isAutoPlay == true) {
+       interval.current = setInterval(() => {
+        offset.value = offset.value + width
+       }, 2000); 
+    }else {
+      clearInterval(interval.current);
+    }
+
+    return () => {
+      clearInterval(interval.current);
+    }
+
+  }, [isAutoPlay, offset, width]);
+
+  useDerivedValue(() => {
+    scrollTo(ref, offset.value, 0, true)
+  });
 
   const onScrollHandler = useAnimatedScrollHandler({
     onScroll: (e) => {
       scrollX.value = e.contentOffset.x;
+    },
+    onMomentumEnd: (e) => {
+      offset.value = e.contentOffset.x;
     }
   });
 
@@ -38,6 +68,7 @@ const Slider = ({ moviesSlider }: Props) => {
   return (
     <View>
       <Animated.FlatList
+        ref={ref}
         data={data}
         renderItem={({ item, index }) => <SliderItem item={item} index={index} scrollX={scrollX} />}
         horizontal
@@ -49,6 +80,12 @@ const Slider = ({ moviesSlider }: Props) => {
         viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
         onEndReached={() => setData([...data, ...moviesSlider])}
         onEndReachedThreshold={0.5}
+        onScrollBeginDrag={() => {
+          setIsAutoPlay(false);
+        }}
+        onScrollEndDrag={() => {
+          setIsAutoPlay(true);
+        }}
       />
       <Pagination items={moviesSlider} paginationIndex={paginationIndex} scrollX={scrollX} />
     </View>
